d48c19008701ce7cb8bef6526917019c71986a11
yellowmars <po0905116@gmail.com>
2024-03-15 11:34:59 +0800

    week04

---
+++ week04/week04-2.cpp
@@ -0,0 +1,8 @@
+# include <stdio.h>
+int main()
+{
+  for(int i=1; i<=3; i++){
+    printf("i¬O:%d\n",i);
+  }
+
+}
---
+++ week04/week04-2.py
@@ -0,0 +1,6 @@
+#week04-3  for迴圈 比較複新的參數
+for i in range(1,9+1,+1): #1...9 #最後的+1可省略
+  print('i是',i,end=' ')
+print() #跳行
+for i in range(9,1-1,-1): #9...1
+  print('i是',i,end=' ')
