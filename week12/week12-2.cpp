5ee5b0bb0a8f6ceb1554a9a84777d113d807d28b
yellowmars <po0905116@gmail.com>
2024-05-10 11:53:53 +0800

    week12

---
+++ week12/week12-2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n=2;
+    int a[2][2]={ {10,20},{11,22} };
+    int b[2][2]={ {2,1},{3,2} };
+    int c[2][2];
+    ///上週教的 part1 讀資料。先設好，就不用再讀
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%3d ",a[i][j]);
+        }
+        printf("\n");
+    }
+}
